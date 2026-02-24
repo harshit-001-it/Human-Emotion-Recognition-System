815d19d4fe84f74766d47c8b63e614293fd37cc7
harshit-001-it <harshitmishra1208@gmail.com>
2026-02-24 11:02:28 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import subprocess
+import sys
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+required_packages = {
+    'flask': 'flask',
+    'cv2': 'opencv-python',
+    'numpy': 'numpy',
+    'tensorflow': 'tensorflow',
+    'PIL': 'Pillow'
+}
+
+print("Checking dependencies...")
+for module, package in required_packages.items():
+    try:
+        __import__(module)
+    except ImportError:
+        print(f"{module} not found. Installing {package}...")
+        install(package)
+
+from app import app
+import webbrowser
+from threading import Timer
+
+def open_browser():
+    webbrowser.open_new('http://127.0.0.1:5000')
+
+if __name__ == '__main__':
+    Timer(1.5, open_browser).start()
+    app.run(host='0.0.0.0', port=5000, debug=True)
